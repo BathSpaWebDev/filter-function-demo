a45f7827566828fec50238625e06abba9c538cce
OlgitH <berman.oj@gmail.com>
2022-11-10 14:16:10 +0000

    ~

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import { dropdownOptions, dadJokes } from './data';
+import {useState} from 'react';
 
 function App() {
+
+  const [jokes, setJokes] = useState(dadJokes);
+
+  const filterFunction = (e) => {
+    console.log(e.target.value);
+    const filteredJokes = dadJokes.filter((joke,i) => {
+        return joke.funniness === e.target.value;
+    })
+    setJokes(filteredJokes);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Create a dropdown input from a list of data */}
+      {/* Create a filter button, that filters the data by one of the selections in the dropdown */}
+      <div className="filter">
+        <header>
+          <label htmlFor="filter-dropdown">Filter by:</label>
+          <select name="filter-dropdown" id="filter-dropdown" onChange={(e) => filterFunction(e) }> 
+          {
+            dropdownOptions.map((option, i) => {
+              return <option key={`option-${i}`} value={option.id}>{option.text}</option>;
+            })
+          }
+          </select>
+        </header>
+        <main>
+          {
+            jokes.map((item, i) => {
+              console.log(item.genre)
+              return (
+                <div key={i} className="joke">
+                  <h3>{item.joke}</h3>
+                  <p>This joke in under the genre of {item.genre ?item.genre.name : ''}</p>
+                  
+                </div>
+              )
+            })
+          }
+        </main>
+        
+      </div>
     </div>
   );
 }
---
+++ src/data/index.js
@@ -0,0 +1,70 @@
+export const dropdownOptions = [
+    { id: 'quite_funny', text: 'Quite funny' },
+    { id: 'moderately_unfunny', text: 'Moderately unfunny' },
+    { id: 'really_unfunny', text: 'Really unfunny' }
+];
+
+export const dadJokes = [
+    {
+      id: "1T01LBXLuzd",
+      joke: "Why does a Moon-rock taste better than an Earth-rock? Because it's a little meteor.",
+      status: 200,
+      funniness: "quite_funny",
+      genre: {name: "moon_jokes", id: 1}
+    },
+    {
+      id: "vPmy5EtPKuc",
+      joke: "Do you want a brief explanation of what an acorn is? In a nutshell, it's an oak tree.",
+      status: 200,
+      funniness: "moderately_unfunny",
+      genre: { name: "tree_jokes", id: 2}
+    },
+    {
+      id: "XLRfNuPZDAd",
+      joke: "What is a tornado's favorite game to play? Twister!",
+      status: 200,
+      funniness: "moderately_unfunny"
+    },
+    {
+      id: "ahqjV82LBAd",
+      joke: "I was thinking about moving to Moscow but there is no point Russian into things.",
+      status: 200,
+      funniness: "moderately_unfunny"
+    },
+    {
+      id: "8prWvkOf2Ed",
+      joke: `What's the difference between a guitar and a fish? You can tune a guitar but you can't "tuna" fish!`,
+      status: 200,
+      funniness: "really_unfunny"
+    },
+    {
+      id: "wX0gNuXgVnb",
+      joke: "What did the Buffalo say to his little boy when he dropped him off at school? Bison.",
+      status: 200,
+      funniness: "really_unfunny"
+    },
+    {
+      id: "s4Me29U0gqc",
+      joke: `Cosmetic surgery used to be such a taboo subject.Now you can talk about Botox and nobody raises an eyebrow.`,
+      status: 200,
+      funniness: "quite_funny"
+    },
+    {
+      id: "y59pGlbhyAd",
+      joke: "I really want to buy one of those supermarket checkout dividers, but the cashier keeps putting it back.",
+      status: 200,
+      funniness: "moderately_unfunny"
+    },
+    {
+      id: "8prWvkOf2Ed",
+      joke: `What's the difference between a guitar and a fish? You can tune a guitar but you can't "tuna" fish!`,
+      status: 200,
+      funniness: "really_unfunny"
+    },
+    {
+      id: "5oGYLZo4Tvc",
+      joke: "I used to be addicted to soap, but I'm clean now.",
+      status: 200,
+      funniness: "moderately_unfunny"
+    }
+  ]
